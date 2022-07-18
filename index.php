bc239f34a2d10a20e21d885539962cf32b7682a1
Iwansaepudin <109509054+Iwansaepudin@users.noreply.github.com>
2022-07-18 15:02:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Hi, Welcome in my contact data!</p>
+</div>
+
+<?=template_footer()?>
